1f9e7612c5b26cdcb901c931924cfbddabb2e32f
Brick <bricks9711@outlook.com>
2024-07-27 01:25:13 +0800

    chapter 9 finished

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,7 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
+            priority={true}
           />
           <Image
             src="/hero-mobile.png"
